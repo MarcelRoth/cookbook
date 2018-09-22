9b17e5c02d4bf52732e01df4d3c7570a798c6071
Marcel Roth <marcel.roth@tngtech.com>
2018-09-22 15:19:24 +0200

    introduce JS

---
+++ web/static/js/application.js
@@ -0,0 +1,18 @@
+function myAlert() {
+    alert("Hello! I am an alert box!!");
+}
+
+function initialize() {
+    var $form = $("form#recipe_search");
+    console.log($form);
+debugger;
+    $form.submit(function(event) {
+    debugger;
+        console.log("triggered")
+        myAlert();
+        event.preventDefault();
+    })
+}
+
+
+$( document ).ready(initialize)
